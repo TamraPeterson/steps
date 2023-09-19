63037fff0bdb24265eec102da19c0372d8b378ba
Tamra Peterson <tamrap@engageusdev.onmicrosoft.com>
2023-09-19 11:45:32 -0600

    count challenge added

--- src/App.js
+++ src/App.js
@@ -7,42 +7,100 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Counter />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((curStep) => curStep - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((curStep) => curStep + 1);
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleStepMinus() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleStepPlus() {
+    setStep((s) => s + 1);
+  }
+
+  function handleCountMinus() {
+    setCount((c) => c - step);
+  }
+
+  function handleCountPlus() {
+    setCount((c) => c + step);
+  }
+
+  return (
+    <>
+      <div className="counter">
+        <button onClick={handleStepMinus}>-</button>
+        <h4>Step: {step}</h4>
+        <button onClick={handleStepPlus}>+</button>
+      </div>
+      <div className="counter">
+        <button onClick={handleCountMinus}>-</button>
+        <h4>Count: {count}</h4>
+        <button onClick={handleCountPlus}>+</button>
+      </div>
+      <h4 className="counter">
+        {count} days from today is {date.toDateString()}
+      </h4>
+    </>
   );
 }
